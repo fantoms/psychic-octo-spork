e9b761dcee16e7c8cc3c3f81ccc95fba0f1d0b72
fantoms <fantom.stranger@gmail.com>
2016-05-27 17:22:57 -0500

    -Removed test code

---
+++ worker-scripts/set-remote-swap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#zookeeper on w01 and w02 
+declare -a workers=( "w01" "w02" "w03" "w04" "w05" "w06" "w07" "w08" "w09" "w10" "w11" "w12" "w13" "w14" "w15" "w16" "w17" "w18" "w19" "w20" "w21" "w22" "w23" "w24" "w25" "w26" "w27" "w28" "w29" "w30" "w31" "w32" "w33" "w34" )
+
+#for each worker, we will ssh in and use sudo to append the "vm.swappiness=1" to the end
+for i in "${workers[@]}"
+do
+	ssh -tt $i 'sudo sysctl vm.swappiness=1'
+done
+
