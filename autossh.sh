87e7717e48603a9bc09094754e2507f14a07c4e6
Andrew Hafferman <haff7097@stthomas.edu>
2016-06-30 16:06:25 +0000

    Added ssh option ExitOnForwardFailure to force autossh to reconnect until all tunnels can be established

--- autossh.sh
+++ autossh.sh
@@ -3,6 +3,7 @@
 #good autossh -M 56400 -f -N -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /home/chip/.ssh/id_rsa -R 43200:localhost:22 -L 9092:localhost:9092 user@host &
 #GREAT autossh -M 56400 -f -N -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /home/chip/.ssh/id_rsa -R 43200:localhost:22 -L 9092:localhost:9092 user@host &
 #testing new options:
-autossh -M 56400 -f -N -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "ServerAliveInterval 5" -o "ServerAliveCountMax 2" -i /home/chip/.ssh/id_rsa -R 43200:localhost:22 -L 9092:localhost:9092 user@host &
+#autossh -M 56400 -f -N -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "ServerAliveInterval 5" -o "ServerAliveCountMax 2" -i /home/chip/.ssh/id_rsa -R 43200:localhost:22 -L 9092:localhost:9092 user@host &
+autossh -M 56400 -f -N -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "ServerAliveInterval 5" -o "ServerAliveCountMax 2" -o "ExitOnForwardFailure yes" -i /home/chip/.ssh/id_rsa -R 43200:localhost:22 -L 9092:localhost:9092 user@host &
 
 exit 0
